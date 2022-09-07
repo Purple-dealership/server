bf398d589d1dc557c40ad050da73b0e620b88907
David Suy <DavidSuyDev@gmail.com>
2022-09-07 15:28:08 -0700

    update schema

--- model.js
+++ model.js
@@ -10,6 +10,7 @@ const carSchema = new Schema({
   year: { type: Number, required: true },
   car_img: { type: String, required: false },
   cylinders: { type: Number, required: false },
+  miles: { type: Number, required: false },
   highway_mpg: { type: Number, required: false },
   city_mpg: { type: Number, required: false },
   combination_mpg: { type: Number, required: false },
@@ -19,8 +20,11 @@ const carSchema = new Schema({
   trim: { type: String, required: false },
   displacement: { type: Number, required: false },
   fuel_type: { type: String, required: false },
+  price: { type: Number, required: false },
+  description: { type: String, required: false },
+  featured: { type: Boolean, required: false },
 });
 
 const CarModel = mongoose.model('Cars', carSchema);
 
-module.exports = CarModel; 
+module.exports = CarModel;
